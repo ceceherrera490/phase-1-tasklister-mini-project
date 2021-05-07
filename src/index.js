90852e0ae41cc2997e3458cddfb800d75592ecb7
Crystal <ceceherrera490@gmail.com>
2021-05-06 22:04:23 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+
+const taskForm = document.getElementById("create-task-form");
+const listed = document.getElementById("list");
+
+taskForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log(e);
+  const inputt = e.target.querySelector("#new-task-description").value;
+
+  const listAdd = document.createElement("li");
+  listAdd.textContent = inputt;
+  listed.appendChild(listAdd);
+
+  taskForm.reset();
+
+  listAdd.addEventListener("click", () => {
+    listAdd.style.textDecoration = "line-through";
+  });
+
+  listAdd.addEventListener("dblclick", () => {
+    listed.removeChild(listAdd);
+  });
+});
